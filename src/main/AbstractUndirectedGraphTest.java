3ae28779eece602873ce88a3e4e31ba46d5c7f59
OrangeEye <36265644+OrangeEye@users.noreply.github.com>
2018-09-14 10:48:37 +0200

    Gewichtete Kanten wurden hinzugefügt, Knoten können gelöscht werden. Kürzester Weg zwischen zwei Knoten wird mit Dijkstra Algorithmus berechnet. isConnected(), hasCycles(), isTree(), bfs, dfs sind ungetestet.

--- src/main/AbstractUndirectedGraphTest.java
+++ src/main/AbstractUndirectedGraphTest.java
@@ -4,7 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
 
 public abstract class AbstractUndirectedGraphTest {
 	public abstract IUndirectedGraph<Character> get();
--- src/main/Node.java
+++ src/main/Node.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Node<T> {
